f12386ef1729d02573704145b3e243e3bea19f45
ycw0519 <ycw0519@naver.com>
2024-07-28 23:43:03 +0900

    Feat: 회원가입 기능 구현 이메일 인증 코드 요청 이메일 인증 코드 확인 요청 회원가입 요청

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 
+import Signup from './components/auth/Signup';
+
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return <Signup />;
 }
 
 export default App;
