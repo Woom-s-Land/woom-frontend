015fd0a6380d1398a8445af5f1d103afb5b157de
정훈 <vision981111@naver.com>
2024-07-30 11:45:12 +0900

    Feat:재사용 컴포넌트(모달,버튼)구현

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+function Button({ label, onClick }) {
+  const [isActive, setIsActive] = useState(false);
+
+  // 버튼의 너비를 계산하기 위해 label의 길이에 따라 기본 너비를 설정합니다.
+  const calculateWidth = (text) => {
+    const baseWidth = 50; // 기본 너비
+    const extraWidthPerChar = 10; // 문자당 추가 너비
+    return baseWidth + text.length * extraWidthPerChar;
+  };
+
+  const buttonWidth = calculateWidth(label);
+
+  const handleMouseDown = () => setIsActive(true);
+  const handleMouseUp = () => setIsActive(false);
+
+  return (
+    <button
+      className="relative flex items-center justify-center rounded cursor-pointer overflow-hidden"
+      style={{ width: `${buttonWidth}px`, height: '80px' }} // 버튼의 너비와 높이를 설정합니다.
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
+      onClick={onClick}
+    >
+      <img 
+        src={isActive ? 'assets/square-bt-down.png' : 'assets/square-bt-up.png'} 
+        alt={label}
+        style={{ 
+          width: '100%',  // 버튼의 가로 길이에 맞게 늘림
+          height: '50px', // 버튼의 높이에 맞게 조정 (고정된 높이)
+          objectFit: 'fill', // 이미지의 비율을 무시하고 버튼 영역에 맞게 늘림
+          objectPosition: 'center'
+        }} 
+      />
+      <span 
+        className="absolute text-sm font-bold text-white"
+        style={{ 
+          top: '50%', // 버튼의 중앙으로 위치 조정
+          left: '50%',
+          transform: 'translate(-50%, -60%)', // 정확히 중앙에 위치시키기 위한 변환
+          whiteSpace: 'nowrap' // 문구가 잘리지 않도록 설정
+        }} 
+      >
+        {label}
+      </span>
+    </button>
+  );
+}
+
+export default Button;
---
+++ src/components/common/Modal.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Modal({ children, onClose }) {
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center m-0 p-0">
+      <div className="bg-modal-bg bg-cover bg-center p-0 rounded-lg w-[700px] h-[447px] relative flex flex-col justify-center m-0 p-0">
+        <button className="absolute top-3 left-5 w-6 h-6 bg-close-bt bg-cover" onClick={onClose} />
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
