15b66d151c71a1bd7269066d44bf87f9da1af21d
kmdy <kmdy125@gmail.com>
2024-07-31 10:29:46 +0900

    Feat: 그룹 공간 상호 작용 구현 객체에 다가가면 상호작용 활성화 표시

--- src/world/Map.jsx
+++ src/world/Map.jsx
@@ -1,23 +1,18 @@
 import CharacterInMap from './CharacterInMap';
-import boundary from '../assets/map/map_collisions';
 import mapImages from './mapImages';
-import {
-  initializeCollisionMap,
-  initializeBoundaries,
-} from '../utils/boundaryUtils';
-import { useState, useEffect } from 'react';
-import { Stage, Sprite, Graphics } from '@pixi/react';
+import { useState } from 'react';
+import { Stage, Sprite } from '@pixi/react';
 
 const Map = () => {
   const width = window.innerWidth;
   const height = window.innerHeight;
   const [backgroundX, setBackgroundX] = useState(-300);
   const [backgroundY, setBackgroundY] = useState(-700);
-
+  const [mapImage, setMapImage] = useState(mapImages.map);
   return (
     <div>
       <Stage width={width} height={height}>
-        <Sprite image={mapImages.map} x={backgroundX} y={backgroundY} />
+        <Sprite image={mapImage} x={backgroundX} y={backgroundY} />
         <CharacterInMap
           width={width}
           height={height}
@@ -25,6 +20,7 @@ const Map = () => {
           setBackgroundY={setBackgroundY}
           backgroundX={backgroundX}
           backgroundY={backgroundY}
+          setMapImage={setMapImage}
         />
       </Stage>
     </div>
--- src/world/mapImages.js
+++ src/world/mapImages.js
@@ -1,8 +1,13 @@
 import map from '../assets/map/map.png';
-
+import nearPhoto from '../assets/map/map_interaction_photo.png';
+import nearPhotomap from '../assets/map/map_interaction_photomap.png';
+import nearGuestbook from '../assets/map/map_interaction_guestbook.png';
 const init = {};
 const imageSrc = {
   map,
+  nearPhoto,
+  nearPhotomap,
+  nearGuestbook,
 };
 
 const mapImages = Object.entries(imageSrc).reduce((images, [key, src]) => {
