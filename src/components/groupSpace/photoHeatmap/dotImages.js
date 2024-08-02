f32aed89a05bf3506c413c3b4ef8c90ceb0ea3bd
김도예 <kmdy125@gmail.com>
2024-08-02 11:26:51 +0900

    Feat: 이미지 배열 생성 및 더미 파일 생성

---
+++ src/components/groupSpace/photoHeatmap/dotImages.js
@@ -0,0 +1,36 @@
+import dot from '../../../assets/heatmap/dot.png';
+import dot1_1 from '../../../assets/heatmap/dot1_1.png';
+import dot1_2 from '../../../assets/heatmap/dot1_2.png';
+import dot1_3 from '../../../assets/heatmap/dot1_3.png';
+import dot1_4 from '../../../assets/heatmap/dot1_4.png';
+import dot2_1 from '../../../assets/heatmap/dot2_1.png';
+import dot2_2 from '../../../assets/heatmap/dot2_2.png';
+import dot2_3 from '../../../assets/heatmap/dot2_3.png';
+import dot2_4 from '../../../assets/heatmap/dot2_4.png';
+import dot3_1 from '../../../assets/heatmap/dot3_1.png';
+import dot3_2 from '../../../assets/heatmap/dot3_2.png';
+import dot3_3 from '../../../assets/heatmap/dot3_3.png';
+import dot3_4 from '../../../assets/heatmap/dot3_4.png';
+
+const dotImages = {
+  dot,
+  dot1_1,
+  dot1_2,
+  dot1_3,
+  dot1_4,
+  dot2_1,
+  dot2_2,
+  dot2_3,
+  dot2_4,
+  dot3_1,
+  dot3_2,
+  dot3_3,
+  dot3_4,
+};
+// const dotImages = Object.entries(imageSrc).reduce((images, [key, src]) => {
+//   const image = new Image();
+//   image.src = src;
+//   images[key] = image;
+//   return images;
+// }, init);
+export default dotImages;
