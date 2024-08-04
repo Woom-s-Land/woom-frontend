01144656ce0f8ab05b375f602d384a3f83658988
김도예 <kmdy125@gmail.com>
2024-08-04 19:45:17 +0900

    Design: 공통 모달 컴포넌트 X 위치 조정

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-function Modal({ children }) {  
+function Modal({ children }) {
   const navigate = useNavigate();
 
   const handleKeyDown = (event) => {
@@ -15,10 +15,10 @@ function Modal({ children }) {
       goBack();
     }
   };
-  
+
   const goBack = () => {
     navigate(-1); // 이전 페이지로 이동
-  }
+  };
 
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
@@ -28,13 +28,13 @@ function Modal({ children }) {
   }, []);
 
   return (
-    <div 
-      className="modal-overlay fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center"
+    <div
+      className='modal-overlay fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center'
       onClick={handleOutsideClick}
     >
-      <div className="bg-modal-bg bg-cover bg-center p-0 rounded-lg w-[700px] h-[447px] relative flex flex-col justify-center">
-        <button 
-          className="absolute top-3 left-5 w-6 h-6 bg-close-bt bg-cover" 
+      <div className='bg-modal-bg bg-cover bg-center p-0 rounded-lg w-[700px] h-[447px] relative flex flex-col justify-center'>
+        <button
+          className='absolute top-5 left-7 w-6 h-6 bg-close-bt bg-cover'
           onClick={goBack} // 닫기 버튼 클릭 시 이전 페이지로 이동
         />
         {children}
