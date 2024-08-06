2e0dcde9179adc0f5bc794a2fc5f2aded5d87a29
정훈 <vision981111@naver.com>
2024-08-06 14:19:19 +0900

    Refactor:사진 크기 변경 및 그룹 월별 표시

--- src/components/groupSpace/photo/DetailModal.jsx
+++ src/components/groupSpace/photo/DetailModal.jsx
@@ -11,19 +11,19 @@ function DetailModal({ src }) {
   return (
     <div>
       <Modal>
-        <div className="flex justify-center items-center h-full">
+        <div className="flex justify-center items-center">
           <div
-            className={`relative w-64 h-64 bg-white shadow-lg perspective-1000`} // Removed the class here
+            className={`w-[300px] h-[350px] shadow-lg perspective-1000`} // Removed the class here
             onClick={handleClick}
           >
             <div
-              className={`absolute w-full h-full transition-transform duration-600 ${isFlipped ? 'rotate-y-180' : ''}`}
+              className={`absolute w-[300px] h-[350px] transition-transform duration-600 ${isFlipped ? 'rotate-y-180' : ''}`}
             >
-              <div className={`absolute w-full h-full backface-hidden ${isFlipped ? 'hidden' : ''}`}>
-                <img src={src} alt="Front" className="w-full h-full object-contain" />
+              <div className={`absolute w-[300px] h-[350px] backface-hidden ${isFlipped ? 'hidden' : ''}`}>
+                <img src={src} alt="Front" className="w-[300px] h-[350px] object-contain" />
               </div>
-              <div className={`absolute w-full h-full bg-blue-500 text-white flex items-center justify-center text-2xl rotate-y-180 ${isFlipped ? 'block' : 'hidden'}`}>
-                여기가 뒷면이다
+              <div className={`absolute w-[300px] h-[350px] bg-base-color text-white flex items-center justify-center text-2xl rotate-y-180 ${isFlipped ? 'block' : 'hidden'}`}>
+                정훈이 뒤집었다 이말이다
               </div>
             </div>
           </div>
