272ae5efba42ffbb31e2703a8fa046c211187217
DAEYEONG_YUN <ycw2859@gmail.com>
2024-08-09 00:22:53 +0900

    Config: redux-persist 설정 (#87)

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,12 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import { persistStore, persistReducer } from 'redux-persist';
+import storageSession from 'redux-persist/lib/storage/session';
 import authReducer from './authSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage: storageSession,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
   },
   devTools: process.env.NODE_ENV !== 'production',
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
