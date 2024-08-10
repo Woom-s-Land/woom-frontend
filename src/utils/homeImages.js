c917d31e5b053261d413b10ad9a91c6d030f49b4
김도예 <kmdy125@gmail.com>
2024-08-10 19:36:45 +0900

    Assets: 상호작용 객체 에셋 추가

--- src/utils/homeImages.js
+++ src/utils/homeImages.js
@@ -1,18 +1,20 @@
 import home from '../assets/home/home.png';
-import forwardHome from '../assets/home/forward_home.png';
 import bed from '../assets/home/home-bed.png';
 import desk from '../assets/home/home-desk.png';
 import toilet from '../assets/home/home-toilet.png';
 import forward from '../assets/home/home-forward.png';
+import interactionKey from '../assets/home/interaction_key.png'
+import rug from '../assets/home/home-rug.png'
 
 const init = {};
 const imageSrc = {
   home,
-  forwardHome,
   bed,
   desk,
   toilet,
   forward,
+  interactionKey, 
+  rug
 };
 
 const homeImages = Object.entries(imageSrc).reduce((images, [key, src]) => {
