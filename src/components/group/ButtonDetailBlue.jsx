46011cbdd11cada3a4857d7609f1bc5ca8e15419
김도예 <kmdy125@gmail.com>
2024-08-11 01:54:09 +0900

    Rename: 버튼 파일명 변경

---
+++ src/components/group/ButtonDetailBlue.jsx
@@ -0,0 +1,15 @@
+const ButtonDetailBlue = ({ buttonText, handleClick }) => {
+  return (
+    <button
+      onClick={handleClick}
+      style={{
+        backgroundSize: '100% 100%', // 세로 높이를 100%로 고정하고 가로 폭에 맞게 조정
+        backgroundColor: 'transparent',
+      }}
+      className='active:bg-gr-btn-md-blue-active z-20 bg-gr-btn-md-blue bg-contain whitespace-nowrap text-xxs bg-no-repeat bg-center py-2 px-2 mr-1'
+    >
+      {buttonText}
+    </button>
+  );
+};
+export default ButtonDetailBlue;
