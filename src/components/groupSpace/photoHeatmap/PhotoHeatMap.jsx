d845dca9661f0e7e377fb61d39c277e0a532fc06
김도예 <kmdy125@gmail.com>
2024-08-12 02:48:17 +0900

    Feat: heatmap 관련 기능 구현 및 상호작용 컴포넌트에 woomsId 전달

--- src/components/groupSpace/photoHeatmap/PhotoHeatMap.jsx
+++ src/components/groupSpace/photoHeatmap/PhotoHeatMap.jsx
@@ -1,15 +1,84 @@
-import { useEffect } from 'react';
-import data from './dummy';
+import { useEffect, useState } from 'react';
 import dotImages from './dotImages';
+import basemap from './basemap';
+import data from './dummy';
+import refresh from '../../../assets/heatmap/refresh.png';
 import emptyImg from '../../../assets/heatmap/empty.png';
+import PhotoApi from '../../../apis/PhotoApi';
+import GroupApi from '../../../apis/GroupApi';
 
 const imgList = {
-  0: [dotImages.dot1_1, dotImages.dot1_2, dotImages.dot1_3, dotImages.dot1_4],
-  1: [dotImages.dot2_1, dotImages.dot2_2, dotImages.dot2_3, dotImages.dot2_4],
-  2: [dotImages.dot3_1, dotImages.dot3_2, dotImages.dot3_3, dotImages.dot3_4],
+  RED: [dotImages.dot1_1, dotImages.dot1_2, dotImages.dot1_3, dotImages.dot1_4],
+  GREEN: [
+    dotImages.dot2_1,
+    dotImages.dot2_2,
+    dotImages.dot2_3,
+    dotImages.dot2_4,
+  ],
+  BLUE: [
+    dotImages.dot3_1,
+    dotImages.dot3_2,
+    dotImages.dot3_3,
+    dotImages.dot3_4,
+  ],
 };
-const PhotoHeatMap = ({ onClose }) => {
-  const groupColor = 1;
+
+const PhotoHeatMap = ({ onClose, woomsId }) => {
+  const [photoData, setPhotoData] = useState();
+  const [groupColor, setGroupColor] = useState('RED');
+  const [error, setError] = useState();
+  const [isAdmin, setIsAdmin] = useState(false);
+  const colors = ['RED', 'GREEN', 'BLUE'];
+
+  const isAdminFunc = async () => {
+    try {
+      const data = await GroupApi.isAdmin(woomsId);
+      setIsAdmin(data.woomsAdmin);
+    } catch (err) {
+      console.error('Failed to check admin status:', err);
+      setError('Failed to check admin status.');
+    }
+  };
+
+  const getColor = async () => {
+    try {
+      const data = await GroupApi.getGroupInfo(woomsId);
+      setGroupColor(data.woomsDto.mapColorStatus);
+    } catch (err) {
+      setError('fail get group color');
+    }
+  };
+  const [heatmap, setHeatmap] = useState();
+
+  const generateHeatmap = (basemap, apiData) => {
+    const apiDataMap = apiData.reduce((acc, { id, count }) => {
+      acc[id] = count;
+      return acc;
+    }, {});
+
+    return basemap.map((row) =>
+      row.map((cell) =>
+        cell === -1 ? -1 : apiDataMap[cell] !== undefined ? apiDataMap[cell] : 0
+      )
+    );
+  };
+
+  const getMap = async () => {
+    if (!woomsId) return;
+    try {
+      const data = await PhotoApi.getMapInfo(woomsId);
+
+      setHeatmap(generateHeatmap(basemap, data));
+    } catch (err) {
+      setError('fail get photo info');
+    }
+  };
+
+  useEffect(() => {
+    getColor();
+    isAdminFunc();
+    getMap();
+  }, [woomsId]);
 
   const getImageSrc = (value) => {
     switch (true) {
@@ -25,15 +94,38 @@ const PhotoHeatMap = ({ onClose }) => {
         return imgList[groupColor][3];
     }
   };
+
+  const handleClose = () => {
+    saveColorToServer(groupColor); // 색상 서버에 저장
+    onClose(); // 모달 닫기
+  };
+
   const handleKeyDown = (e) => {
     if (e.key === 'Escape') {
-      onClose();
+      handleClose();
     }
   };
 
   const handleOutsideClick = (e) => {
     if (e.target.classList.contains('modal-overlay')) {
-      onClose();
+      handleClose();
+    }
+  };
+
+  const handleColorClick = async () => {
+    const currentIndex = colors.indexOf(groupColor);
+
+    const nextIndex = (currentIndex + 1) % colors.length;
+    const nextColor = colors[nextIndex];
+
+    setGroupColor(nextColor); // 상태 업데이트
+  };
+
+  const saveColorToServer = async (color) => {
+    try {
+      await GroupApi.changeGroupColor(woomsId, color);
+    } catch (err) {
+      setError('fail to change color');
     }
   };
 
@@ -50,11 +142,17 @@ const PhotoHeatMap = ({ onClose }) => {
       onClick={handleOutsideClick}
       className='modal-overlay fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center'
     >
-      <div className='bg-modal-vertical bg-cover p-5 bg-center w-[470px] h-[550px] relative flex flex-col items-center'>
+      <div className='bg-modal-vertical bg-cover p-5 bg-center w-[520px] h-[600px] relative flex flex-col items-center'>
         <button
-          onClick={onClose}
+          onClick={handleClose}
           className='absolute top-8 left-8 w-6 h-6 bg-close-bt bg-cover'
         />
+        {isAdmin && (
+          <button
+            onClick={handleColorClick}
+            className='absolute bottom-11 right-[80px] bg-contain bg-hm-refresh z-10 w-5 h-5 bg-no-repeat'
+          />
+        )}
         <div className='absolute top-7 flex justify-center items-center'>
           <p className='text-3xl mt-1 text-base-color'>채 움</p>
           <div className='w-4' />
@@ -67,18 +165,19 @@ const PhotoHeatMap = ({ onClose }) => {
         </div>
 
         <div className='w-full flex justify-center mt-8'>
-          <div className='w-[350px] h-[450px] mt-5 bg-heatmap-bg bg-center bg-cover flex justify-center items-center'>
-            <div className='grid grid-cols-custom-19 gap-0.5 mr-4 mt-4'>
-              {data.map((row, rowIndex) =>
-                row.map((cell, cellIndex) => (
-                  <img
-                    key={`${rowIndex}-${cellIndex}`}
-                    src={cell === -1 ? emptyImageSrc : getImageSrc(cell)}
-                    className='w-4 h-4'
-                    alt={`cell-${rowIndex}-${cellIndex}`}
-                  />
-                ))
-              )}
+          <div className='w-[420px] h-[520px] mt-5 bg-heatmap-bg bg-center bg-cover flex justify-center items-center'>
+            <div className='grid grid-cols-custom-19 gap-0.5 ml-6 mt-40'>
+              {heatmap &&
+                heatmap.map((row, rowIndex) =>
+                  row.map((cell, cellIndex) => (
+                    <img
+                      key={`${rowIndex}-${cellIndex}`}
+                      src={cell === -1 ? emptyImageSrc : getImageSrc(cell)}
+                      className='w-[10px] h-[10px]'
+                      alt={`cell-${rowIndex}-${cellIndex}`}
+                    />
+                  ))
+                )}
             </div>
           </div>
         </div>
