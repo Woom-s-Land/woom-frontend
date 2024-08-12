1d32ae2d4a1a261ee711702c6e2fc86f9f1b26e0
김도예 <kmdy125@gmail.com>
2024-08-12 14:10:18 +0900

    Assets: 라디오 관련 에셋 추가

--- src/utils/mapImages.js
+++ src/utils/mapImages.js
@@ -2,13 +2,14 @@ import map from '../assets/map/map.png';
 import photo from '../assets/map/map-photo.png';
 import photomap from '../assets/map/map-photomap.png';
 import guestbook from '../assets/map/map-board.png';
-
+import radio from '../assets/map/map-radio.png';
 const init = {};
 const imageSrc = {
   map,
   photo,
   photomap,
   guestbook,
+  radio,
 };
 
 const mapImages = Object.entries(imageSrc).reduce((images, [key, src]) => {
