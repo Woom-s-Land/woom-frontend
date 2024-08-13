96022b13ce7cd3925c97793d248ff4d28413ec56
DAEYEONG_YUN <ycw2859@gmail.com>
2024-08-14 00:45:05 +0900

    Style: 메인, 로그인, 회원가입 페이지 디자인 변경 (#164)

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,39 +1,153 @@
 import { useNavigate } from 'react-router-dom';
 
+import map from './assets/map/map.png';
+import imgLogo from './assets/logo/imgLogo.png';
+import letterLogo from './assets/logo/letterLogo.png';
+import introImg1 from './assets/mainPage/introImg1.png';
+import introImg2 from './assets/mainPage/introImg2.png';
+import introImg3 from './assets/mainPage/introImg3.png';
+
+import letterImg from './assets/mainPage/letterImg.png';
+import photoMapImg from './assets/mainPage/photoMapImg.png';
+
 const MainPage = () => {
   const navigate = useNavigate();
 
-  const handleClick = (url) => () => {
+  const handleClick = (url) => {
     navigate(url);
   };
 
   return (
     <div>
-      <button onClick={handleClick('/home')}>마이홈으로</button>
-      <br />
-      <button onClick={handleClick('/signup')}>회원가입</button>
-      <br />
-      <button onClick={handleClick('/map')}>그룹 공간으로</button>
-      <br />
-      <button onClick={handleClick('/login')}>로그인</button>
-      <br />
-      <button onClick={handleClick('/photo')}>사진 모달</button>
-      <br />
-      <button onClick={handleClick('/comment')}>방명록 모달</button>
-      <br />
-      <button onClick={handleClick('/heatmap')}>사진 지도</button>
-      <br />
-      <button onClick={handleClick('/group')}>그룹 목록</button>
-      <br />
-      <button onClick={handleClick('/creategroup')}>그룹 생성</button>
-      <br />
-      <button onClick={handleClick('/group-detail')}>그룹 상세</button>
-      <br />
-      <button onClick={handleClick('/loading')}>기본 로딩</button>
-      <br />
-      <button onClick={handleClick('/story-write')}>사연작성 모달</button>
-      <br />
-      <button onClick={handleClick('/story-read')}>사연읽기 모달</button>
+      <div className='flex h-full flex-col'>
+        <header className='flex items-center justify-between border-b border-b-[#F4EFE6] px-10'>
+          <div className='flex items-center gap-4 text-[#1C160C]'>
+            <div className='w-28'>
+              <img src={imgLogo} alt='imgLogo' />
+            </div>
+            <div className='w-48'>
+              <img src={letterLogo} alt='imgLogo' />
+            </div>
+          </div>
+          <div className='flex flex-1 justify-end gap-8'>
+            <button
+              onClick={() => handleClick('/login')}
+              className='flex h-10 min-w-[84px] max-w-[480px] items-center justify-center px-4 font-bold leading-normal tracking-[0.015em] py-2 border border-transparent rounded-xl shadow-sm text-sm text-white bg-green-400 hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500'
+            >
+              <span className='truncate'>로그인</span>
+            </button>
+          </div>
+        </header>
+        <div className='px-40 flex flex-1 justify-center py-5'>
+          <div className='flex flex-col max-w-[960px] flex-1'>
+            <div className='@container'>
+              <div className='@[480px]:p-4'>
+                <div
+                  className='flex min-h-[480px] flex-col gap-6 bg-cover bg-center bg-no-repeat @[480px]:gap-8 @[480px]:rounded-xl items-start justify-end px-4 pb-10 @[480px]:px-10 rounded-xl'
+                  style={{
+                    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.4) 100%), url(${map})`,
+                  }}
+                >
+                  <h1 className='text-white text-4xl font-black leading-tight tracking-[-0.033em] @[480px]:text-5xl text-center'>
+                    WOOMS에서 따뜻함을 느껴보세요
+                  </h1>
+                  <button
+                    onClick={() => handleClick('/signup')}
+                    className='flex h-10 min-w-[84px] max-w-[480px] items-center justify-center px-4 font-bold leading-normal tracking-[0.015em] py-2 border border-transparent rounded-xl shadow-sm text-sm text-white bg-green-400 hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500
+                    '
+                  >
+                    <span className='truncate'>회원가입</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className='flex flex-col gap-10 px-4 py-10 @container text-center'>
+              <h1 className='text-[#1C160C] text-[32px] font-bold leading-tight @[480px]:text-4xl max-w-[720px] mx-auto'>
+                WOOMS의 다양한 기능을 만나보세요!
+              </h1>
+              <div className='grid grid-cols-[repeat(auto-fit,minmax(158px,1fr))] gap-3'>
+                {[
+                  {
+                    title: '사진 픽셀 변환',
+                    description: '사진을 픽셀 아트로 변환해보세요',
+                    image:
+                      'https://cdn.usegalileo.ai/sdxl10/4be7f934-a1be-467c-980a-b7fff7c05ca4.png',
+                  },
+                  {
+                    title: '사진 지도',
+                    description: '어디에서 사진을 많이 찍었을까요?',
+                    image: photoMapImg,
+                  },
+                  {
+                    title: '편지',
+                    description: '따뜻한 마음을 전달해보세요',
+                    image: letterImg,
+                  },
+                  {
+                    title: '라디오',
+                    description: '내 이야기를 사연으로 남겨보세요',
+                    image:
+                      'https://cdn.usegalileo.ai/sdxl10/e45856cc-bf86-4cf2-806c-768769c31dd8.png',
+                  },
+                ].map((feature, idx) => (
+                  <div key={idx} className='flex flex-col gap-3 pb-3'>
+                    <div
+                      className='w-full bg-center bg-no-repeat aspect-video bg-cover rounded-xl'
+                      style={{ backgroundImage: `url(${feature.image})` }}
+                    ></div>
+                    <div>
+                      <p className='text-[#1C160C] text-base font-medium leading-normal'>
+                        {feature.title}
+                      </p>
+                      <p className='text-[#A18249] text-sm font-normal leading-normal'>
+                        {feature.description}
+                      </p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className='grid grid-cols-[repeat(auto-fit,minmax(158px,1fr))] gap-3 p-4'>
+              {[introImg1, introImg2, introImg3].map((image, idx) => (
+                <div key={idx} className='flex flex-col gap-3'>
+                  <div
+                    className='w-full bg-center bg-no-repeat aspect-video bg-cover rounded-xl'
+                    style={{ backgroundImage: `url(${image})` }}
+                  ></div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <footer className='flex justify-center border border-u-[#F4EFE6]'>
+          <div className='flex max-w-[960px] flex-1 flex-col'>
+            <footer className='flex flex-col gap-6 px-5 py-3 text-center @container'>
+              <div className='flex flex-wrap items-center justify-center gap-6 @[480px]:flex-row @[480px]:justify-around'>
+                <span className='text-[#A18249] text-base font-normal leading-normal min-w-20'>
+                  송도언
+                </span>
+                <span className='text-[#A18249] text-base font-normal leading-normal min-w-20'>
+                  홍성우
+                </span>
+                <span className='text-[#A18249] text-base font-normal leading-normal min-w-20'>
+                  이현수
+                </span>
+                <span className='text-[#A18249] text-base font-normal leading-normal min-w-20'>
+                  김도예
+                </span>
+                <span className='text-[#A18249] text-base font-normal leading-normal min-w-20'>
+                  정훈
+                </span>
+                <span className='text-[#A18249] text-base font-normal leading-normal min-w-20'>
+                  윤대영
+                </span>
+              </div>
+            </footer>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 };
