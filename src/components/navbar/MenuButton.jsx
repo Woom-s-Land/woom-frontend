4ffd28646815310c07bc3920faa06b68e6e24706
김도예 <kmdy125@gmail.com>
2024-08-14 05:26:07 +0900

    Feat: 그룹 공간에서 개인 공간으로 이동 (#177)

--- src/components/navbar/MenuButton.jsx
+++ src/components/navbar/MenuButton.jsx
@@ -2,7 +2,7 @@ const MenuButton = ({ buttonText, onClickEventHandler }) => {
   return (
     <button
       onClick={onClickEventHandler}
-      className='mx-3 whitespace-nowrap active:bg-gr-btn-active bg-gr-btn bg-contain bg-no-repeat bg-center p-5 text-center text-2xl text-base-color'
+      className='mx-3 mt-2 whitespace-nowrap active:bg-gr-btn-active bg-gr-btn bg-contain bg-no-repeat bg-center px-5 py-3 text-center text-2xl text-base-color'
     >
       {buttonText}
     </button>
