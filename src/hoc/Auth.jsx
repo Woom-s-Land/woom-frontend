ccca01473d757b25ee5e1e0511ef61e9b1718f6c
이현수 <znight1020@naver.com>
2024-08-14 15:34:39 +0900

    Feat : URL 입력 접근, 그룹 공간 접근 제한

--- src/hoc/Auth.jsx
+++ src/hoc/Auth.jsx
@@ -2,22 +2,30 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import basicAxios from '../libs/axios/basicAxios';
 import { alertActions } from '../store/alertSlice';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 /**
  *
  * @param specificComponent - 감쌀 컴포넌트
- * @param option            - null: 아무나 출인이 가능한 페이지, true: 로그인한 유저만 출입가능, false: 로그인한 유저는 접속불가능항 페이지
+ * @param option            - null: 아무나 출인이 가능한 페이지, true: 로그인한 유저만 출입가능, false: 로그인한 유저는 접속불가능한 페이지
  * @returns {*}
  *
  */
 export default function (SpecificComponent, option) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
 
   const isLogin = useSelector((state) => state.auth.isAuthenticated);
+  const isInMap = location.pathname.startsWith('/map');
+
+  const storedWoomsId = useSelector((state) => state.group.groupInfo.woomsId);
+  const woomsId = location.pathname.split('/')[2];
 
   function AuthenticationCheck() {
+    console.log(woomsId);
+    console.log(storedWoomsId);
+
     useEffect(() => {
       if (option) {
         if (!isLogin) {
@@ -34,6 +42,16 @@ export default function (SpecificComponent, option) {
             );
             navigate('/');
           });
+        } else if (isLogin && isInMap) {
+          if (storedWoomsId === undefined || storedWoomsId != woomsId) {
+            dispatch(
+              alertActions.showAlert({
+                message: '잘못된 접근입니다. 그룹 목록을 통해 이동해주세요',
+                type: 'ERROR',
+              })
+            );
+            navigate('/home');
+          }
         }
       }
     });
