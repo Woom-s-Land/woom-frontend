dc8114c0918c574071b0502f216410a35976cadb
ycw0519 <ycw0519@naver.com>
2024-08-14 15:35:11 +0900

    홈, 그룹 공간 bgm 재생

--- src/components/bgm/PlayBgm.jsx
+++ src/components/bgm/PlayBgm.jsx
@@ -1,45 +1,89 @@
 import { useEffect, useState } from 'react';
 import { Howl } from 'howler';
 import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
-function PlayBgm({ playOnPage }) {
-  const [sound, setSound] = useState(null);
+function PlayBgm() {
+  const [homeSound, setHomeSound] = useState(null);
+  const [woomsSound, setWoomsSound] = useState(null);
   const isPlaying = useSelector((state) => state.setting.audioIsPlaying);
+  const location = useLocation();
+
+  // 음악을 재생할 페이지 정의
+  const playMusicOnPages = {
+    home: '/home',
+    wooms: '/map/:woomsId',
+  };
+
+  // 현재 경로가 음악을 재생할 페이지와 일치하는지 확인
+  const playOnPage = Object.keys(playMusicOnPages).find((key) => {
+    const path = playMusicOnPages[key];
+    return new RegExp(`^${path.replace(/:\w+/, '\\w+')}($|/)`).test(
+      location.pathname
+    );
+  });
 
   useEffect(() => {
-    const audio = new Howl({
-      src: ['/assets/bgm/bgm.mp3'],
+    const homeAudio = new Howl({
+      src: ['/assets/bgm/homeBGM.mp3'],
       loop: true,
       volume: 0.1,
-      autoplay: true,
+      autoplay: false,
       onload: () => {
-        console.log('BGM is loaded');
+        console.log('Home BGM is loaded');
+      },
+      onloaderror: (id, error) => {
+        console.error('Failed to load Home BGM:', error);
       },
     });
 
-    setSound(audio);
+    const woomsAudio = new Howl({
+      src: ['/assets/bgm/groupBGM.mp3'],
+      loop: true,
+      volume: 0.1,
+      autoplay: false,
+      onload: () => {
+        console.log('Wooms BGM is loaded');
+      },
+      onloaderror: (id, error) => {
+        console.error('Failed to load Wooms BGM:', error);
+      },
+    });
+
+    setHomeSound(homeAudio);
+    setWoomsSound(woomsAudio);
 
     return () => {
-      audio.unload();
+      homeAudio.unload();
+      woomsAudio.unload();
     };
   }, []);
 
   useEffect(() => {
-    if (sound) {
-      if (playOnPage) {
-        sound.play();
+    if (homeSound && woomsSound) {
+      if (playOnPage === 'home') {
+        homeSound.play();
+        woomsSound.pause();
+        woomsSound.seek(0);
+      } else if (playOnPage === 'wooms') {
+        woomsSound.play();
+        homeSound.pause();
+        homeSound.seek(0);
       } else {
-        sound.pause();
-        sound.seek(0);
+        homeSound.pause();
+        homeSound.seek(0);
+        woomsSound.pause();
+        woomsSound.seek(0);
       }
     }
-  }, [playOnPage, sound]);
+  }, [playOnPage, homeSound, woomsSound]);
 
   useEffect(() => {
-    if (sound) {
-      sound.mute(isPlaying);
+    if (homeSound && woomsSound) {
+      homeSound.mute(isPlaying);
+      woomsSound.mute(isPlaying);
     }
-  }, [isPlaying, sound]);
+  }, [isPlaying, homeSound, woomsSound]);
 
   return null;
 }
